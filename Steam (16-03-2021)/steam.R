62fa1e42766c51bb609312ac4ab5c4dfeb8d52a1
j-lillis <johnny_lillis@hotmail.com>
2021-03-21 20:38:10 +0000

    Added annotation

--- Steam (16-03-2021)/steam.R
+++ Steam (16-03-2021)/steam.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(ggridges)
 library(lubridate)
 library(extrafont)
-
+library(here)
 # font for title, game names, and x axis labels from https://www.fontspace.com/press-start-2p-font-f11591
 
 # colours from Tomorrow Night Eighties theme https://github.com/chriskempson/tomorrow-theme
@@ -18,6 +18,9 @@ green = "#99cc99"
 aqua = "#66cccc" 
 blue = "#6699cc" 
 purple = "#cc99cc" 
+
+aqua_light <- "#4e8d8c"
+green_light <- "#6c8b6c"
 
 # import
 games_import <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-16/games.csv') %>%
@@ -54,23 +57,34 @@ top_games <- games %>% group_by(gamename) %>%  summarise(year_total_avg) %>% arr
                group = gamename, 
                fill = gamename
     )) +
+  
     
-    # this was not used in the end - highlights Mar-May to show impact of Covid-19 lockdowns
-    #geom_rect(xmin = as.numeric(games$date[10]), xmax = as.numeric(games$date[8]), 
-    #          ymin = 0, ymax = 11, size = 1.5, 
-    #          fill = comment, 
-    #          alpha = 0.1,
-    #          linetype = 2) +
-    
-    geom_ridgeline(stat = "identity", scale = 8.5,
+    geom_ridgeline(stat = "identity", scale = 9.5,
                    #fill = "#66cccc",
                    color = purple, size = 1,
-                   alpha = 0.55) +
+                   alpha = 1) +
     
     geom_text(aes(label=ifelse(month=="June",paste0(gamename),"")), 
-              position = position_nudge( x = 13, y = 0.3), 
+              position = position_nudge( x = 13, y = 0.35), 
               #nudge_x = -15, nudge_y =  0.3,
               color = yellow, family = "Press Start 2P", size = 3.5, hjust = 0.5) +
+    
+    geom_segment(x = as.numeric(games$date[10]), xend = as.numeric(games$date[8]),
+                 y = 11.2, yend = 11.2, color = comment, linetype = 1) +
+    
+    geom_segment(x = as.numeric(games$date[10]), xend = as.numeric(games$date[10]),
+                 y = 11.213, yend = 11.05, color = comment, linetype = 1) +
+    
+    geom_segment(x = as.numeric(games$date[8]), xend = as.numeric(games$date[8]),
+                 y = 11.213, yend = 11.05, color = comment, linetype = 1) +
+    
+    annotate("text", 
+             x = as.Date("2020-04-01"), 
+             y = 11.6,
+             label = "Many countries entered\nCovid-19 lockdown",
+             color = comment,
+             family = "OCR A Std", size = 3, lineheight = 0.9) +
+    
     
     labs(title = "DISTRIBUTION OF AVERAGE USERS PER \nMONTH FOR TOP 10 MOST POPULAR GAMES\nON STEAM IN 2020",
          subtitle = "For each game I calculated the percentage of total 2020 users that occurred\nin each month, to show the change in popularity across the year. Games are\nranked based on peak month - those which peaked earlier in 2020 are higher\nand those which peaked later are lower.",
@@ -79,7 +93,7 @@ top_games <- games %>% group_by(gamename) %>%  summarise(year_total_avg) %>% arr
     scale_x_date(date_breaks  = "1 months",
                  date_labels  = "%b") +
     
-    scale_fill_cyclical(values = c(green, aqua)) +
+    scale_fill_cyclical(values = c(green_light, aqua_light)) +
     
     scale_y_discrete(expand = c(0,0.2)) +
     
@@ -87,17 +101,24 @@ top_games <- games %>% group_by(gamename) %>%  summarise(year_total_avg) %>% arr
           axis.text.y = element_blank(),
           axis.ticks.y = element_blank(),
           panel.background  = element_blank(),
+          
           panel.grid = element_blank(),
           plot.background = element_rect(fill = background),
           axis.text.x = element_text(color = purple, family = "Press Start 2P", size = 10),
-          plot.title = element_text(color = red, family = "Press Start 2P", lineheight = 1.2, size = 16, hjust = 0.5),
+          plot.title = element_text(color = red, 
+                                    family = "Press Start 2P", 
+                                    lineheight = 1.2, 
+                                    size = 16, 
+                                    hjust = 0.5,
+                                    margin = margin(c(5,0,0,0))),
           plot.title.position = "panel",
-          plot.subtitle  = element_text(color = orange, family = "OCR A Std", size = 10.5, lineheight = 1.1, margin = margin(7,0,12,0), hjust = 0.5),
-          plot.caption = element_text(family = "OCR A Std", size = 10.5, colour = orange)) +    
+          plot.subtitle  = element_text(color = orange, family = "OCR A Std", size = 10.5, lineheight = 1.1, margin = margin(7,0,17,0), hjust = 0.5),
+          plot.caption = element_text(family = "OCR A Std", size = 10.5, colour = orange)) + 
+    coord_cartesian(clip = "off") +
     NULL
 )
 
-png("plot_v1.png", res = 300*2,  width =2500*2, height = 2500*2)
+png(here("Steam (16-03-2021)", "steam_plot.png"), res = 300*2,  width =2500*2, height = 2500*2)
 print(plot)
 dev.off()
 
