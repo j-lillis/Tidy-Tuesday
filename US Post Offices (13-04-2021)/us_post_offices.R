536c28e2840e35c696cbb64ef5e59ab25110e7e7
j-lillis <johnny_lillis@hotmail.com>
2021-05-07 21:05:40 +0100

    added caption, final tweaks

--- US Post Offices (13-04-2021)/us_post_offices.R
+++ US Post Offices (13-04-2021)/us_post_offices.R
@@ -1,12 +1,9 @@
 library(tidyverse)
-library(urbnmapr)
 library(sf)
 library(patchwork)
 library(ggtext)
 library(extrafont)
 library(here)
-
-#font_import()
 
 #### DATA SETUP
 
@@ -25,12 +22,13 @@ post_offices <- post_offices_import  %>%
 discontinued_counts <- post_offices %>%
   count(discontinued)
 
+# the following code and approach based on https://github.com/henrywrover/tidytuesday/blob/master/R/post%20offices.R
+
 post_office_year_counts <- post_offices %>% count(established) %>% 
   inner_join(., discontinued_counts, by = c("established" = "discontinued" )) %>% 
   rename("established" = "n.x",
          "discontinued" = "n.y",
          "year" = "established") %>% 
-  mutate(decade = year - year %% 10) %>% 
   mutate(period = 
            case_when(year <= 1800 ~ "1800",
                      year <= 1825 ~ "1825",
@@ -59,11 +57,12 @@ background <- "#f0efe4"
 font <- "Raleway-Medium"
 font_bold <- "Raleway-SemiBold"
 
-
 theme_update(plot.background = element_rect(fill = background, color = NA),
              plot.margin = margin(rep(5,4)))
 
+
 ####### FUNCTION TO GENERATE INDIVIDUAL MAPS AND BAR PLOTS 
+
 plot_period <- function(.data, period_to_plot){
   
   centre_lon = -115
@@ -89,13 +88,13 @@ plot_period <- function(.data, period_to_plot){
     ) +
     geom_point(aes(x = longitude, y = latitude), size = 0.5, alpha = 0.4, color = usps_blue) +
     coord_fixed(1.25, clip = "off") +
-    annotate(geom = "text", x = centre_lon, y = centre_lat, label = period_to_plot,
+    annotate(geom = "text", x = centre_lon, y = centre_lat, label = period_to_plot, family = font_bold,
              size = 12, fontface = "bold", colour = grey_med, alpha = 0.8) +
-    annotate(geom = "text", x = centre_lon-8, y = centre_lat+4.5, label = paste0(min_period, "-"),
+    annotate(geom = "text", x = centre_lon-8, y = centre_lat+4.5, label = paste0(min_period, "-"), family = font_bold,
              size = 8, fontface = "bold", colour = grey_med, alpha = 0.7)
   
   bar_plot <- .data[[2]] %>% filter(period == period_to_plot) %>% 
-    ggplot(aes(x = 0, y = value, fill = name)) + geom_col() +
+    ggplot(aes(x = 0, y = value, fill = name)) + geom_col(width = 1) +
     theme(panel.background  = element_rect(fill = background),
           axis.text = element_blank(),
           axis.ticks = element_blank(),
@@ -106,26 +105,20 @@ plot_period <- function(.data, period_to_plot){
           panel.grid.minor.y = element_blank(),
           #plot.margin=grid::unit(c(0,0,0,0), "mm"),
           legend.position = "none") +
-    geom_hline(yintercept = 0) +
+    geom_segment(aes(x = -0.5, xend = 0.5, y = 0, yend = 0), color = grey_med_dark) +
     scale_fill_manual(values = c(usps_red, usps_blue)) + 
     
     coord_flip(clip = "off") + ylim(c(-55000,71000))
   
-  
   return(map_plot / bar_plot + plot_layout(heights = c(15,1)))
-  
-  
 }
-
 
 periods <- c("1800", "1825", "1850", "1875", "1900", "1925")
 
 period_plots <- periods %>% map(plot_period, .data = post_offices_list)
 
-###### LARGE MAP
+###### GENERATE CHART OF FIRST POST OFFICE BY EACH STATE
 
-
-# first post office by state
 state_lookup <- tibble::tribble(~state_name, ~state_abb,
                                 "ALABAMA",  "AL",
                                 "ALASKA", "AK",
@@ -192,24 +185,6 @@ state_established <- post_offices %>%
   slice(n=1) %>% arrange(established) %>% select(state, established) %>% 
   inner_join(., state_lookup, by = c("state" = "state_abb"))
 
-states_map <- us_map %>% left_join(., state_established, by = c("region" = "state_name")) %>% 
-  mutate(rank = dense_rank(established)) %>% 
-  
-  ggplot() +
-  geom_polygon(aes(x= long, y = lat, group = group, fill = rank), 
-               colour = "#E8E8E8",
-               size = 0.2) +
-  theme(panel.background  = element_rect(fill = NULL),
-        axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title = element_blank(),
-        plot.title = element_text(color = grey_accent, face = "bold", size = 40, hjust = 0.5,
-                                  margin = margin(30,20,0,0)),
-        panel.grid = element_blank(),
-        legend.position = "none"
-        #plot.margin=grid::unit(c(0,0,0,0), "mm")
-  )
-
 state_timeline <- state_established %>% 
     group_by(established) %>% 
     mutate(rank = dense_rank(state_name)) %>% 
@@ -224,29 +199,33 @@ state_timeline <- state_established %>%
           panel.background = element_rect(fill = background),
           panel.grid.major.x = element_blank(),
           panel.grid.minor.x = element_blank(),
-          panel.grid.minor.y = element_blank(),
-          legend.position = "none",
-          panel.grid.major.y = element_line(color = grey_light),
-          axis.text.y = element_text(family = font, color = grey_accent, size = 10),
+          panel.grid.minor.y = element_line(color = grey_light, size = 0.5),
+          panel.grid.major.y = element_line(color = grey_light, size = 0.5),
+          axis.text.y = element_text(family = font, color = grey_med_dark, size = 14),
           plot.subtitle  = element_text(family = font, color = grey_accent, size = 25,
                                     margin = margin(20,0,0,0)),
-          plot.title.position = "plot"
+          plot.title.position = "plot",
+          legend.position = "none"
     ) +
     
     scale_color_manual(values = c(usps_blue, usps_red)) +
-    scale_y_reverse(breaks = seq(1650, 1900, 25),
-                    sec.axis = sec_axis(~., breaks = seq(1650, 1900, 25))) +
+    scale_y_reverse(breaks = seq(1650, 1900, 50), minor_breaks = seq(1650, 1900, 25),
+                    sec.axis = sec_axis(~., breaks = seq(1650, 1900, 50))) +
     labs(subtitle   = "Year the first post office was established in each state") 
 
+
+####### FINAL ASSEMBLY OF PLOTS
 
 period_plots_merged <- (period_plots[[1]] | period_plots[[2]] | period_plots[[3]]) / 
   (period_plots[[4]] | period_plots[[5]] | period_plots[[6]])
 
 (period_plots_merged / state_timeline) + 
   plot_annotation(title = "A history of post offices in the United States",
-                  subtitle = "Dots show total number of post offices established by the end of the period<br>Bars show number of post offices <span style = 'color:#004B87;'>established</span> and <span style = 'color:#DA291C;'>discontinued</span> within each period") & 
+                  subtitle = "Dots show every post office established by the end of each period<br>Bars show the number of post offices <span style = 'color:#004B87;'>established</span> and <span style = 'color:#DA291C;'>discontinued</span> within each period",
+                  caption = "Plot created by Johnny Lillis | github.com/j-lillis | twitter.com/johnny_c_lillis | Data from Cameron Blevins & Richard W. Helbock") & 
   theme(plot.title  = element_text(family = font_bold, size = 48, color = grey_accent, margin = margin(10,40,7,20)),
         plot.subtitle  = element_markdown(family = font_bold, size = 22, color = grey_med_dark, lineheight = 1.3),
+        plot.caption = element_text(family = font, size = 10, color = grey_med_dark),
         panel.background  = element_rect(fill = background, color = NA))
 
 ggsave(filename =  paste0("temp-", format(Sys.time(), "%Y%m%d_%H%M%S"), ".png"), 
