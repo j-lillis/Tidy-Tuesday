d60b31c6f89153f5ce682dc0bd4ca03f02446428
Johnny Lillis <johnny_lillis@hotmail.com>
2021-07-02 01:06:58 +0100

    added intro text

--- Ask A Manager survey (18-05-2021)/ask_a_manager.R
+++ Ask A Manager survey (18-05-2021)/ask_a_manager.R
@@ -6,10 +6,9 @@ library(ggtext)
 library(extrafont)
 library(here)
 
-#### to do: add fonts, add titles + subtitles (use element_markdown to include these both within subtitle),
-# add scales formatting to x axis, add caption, 
-# convert currencies to USD
-# use geom_text to add in base size for each density ???
+#### to do: add top left panel with background on survey (date, n, etc) and trends in data
+#### add bottom left panel with comment on top industries
+#### add caption to bottom with my details and link to data
 
 df_import <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-05-18/survey.csv')
 
@@ -54,13 +53,14 @@ df_filt <- df_import %>%
 
 # plot setup
 
-background <- "black"
+background <- ghibli_palettes$PonyoDark[1]
 text_colour <- ghibli_palettes$PonyoLight[2]
 yellow <- ghibli_palettes$PonyoMedium[6]
 blue <- ghibli_palettes$PonyoMedium[3]
 
 font_bold <- "Roboto-Bold"
 font_regular <- "Roboto-Regular"
+
 #### create plot function
 
 density_dumbbell_plot <- function(.data, y_var, sort = F, pct_change_text = T) {
@@ -121,12 +121,12 @@ density_dumbbell_plot <- function(.data, y_var, sort = F, pct_change_text = T) {
         colour = alpha(yellow, 0.5),
         family = font_regular) } +
     
-    xlim(c(0,250000)) +
-    
     scale_fill_manual(values = c(yellow, blue),
                       aesthetics = c("colour", "fill")) +
     
     scale_y_discrete(labels = function(x) str_wrap(x, width = 10)) +
+    
+    scale_x_continuous(limits = c(0, 250000), labels = scales::label_dollar()) +
     
     
     theme(plot.background  = element_rect(fill = background, colour = background),
@@ -143,7 +143,7 @@ density_dumbbell_plot <- function(.data, y_var, sort = F, pct_change_text = T) {
           axis.ticks = element_blank(),
           axis.title = element_blank(),
           panel.grid.major.y = element_blank(),
-          panel.grid.major.x = element_line(colour = alpha(ghibli_palettes$PonyoMedium[2], 0.1), size = 1.5),
+          panel.grid.major.x = element_line(colour = alpha(ghibli_palettes$PonyoMedium[2], 0.1), size = 0.5),
           panel.grid.minor.x = element_blank(),
           panel.grid.minor.y = element_blank(),
           
@@ -159,45 +159,7 @@ density_dumbbell_plot <- function(.data, y_var, sort = F, pct_change_text = T) {
 
 
 
-tribble(~annual_salary, ~gender, ~fake_var
-)
 
-male_dist <- seq(-4,4,length = 1000)
-female_dist <- seq(-5,3, length = 1000) 
-
-reps <- 1000000
-
-dummy_dists <- tibble(annual_salary = c(rnorm(reps,3),
-                                        rnorm(reps,2.2)), 
-                      gender = factor(c(rep("Man", reps), rep("Woman", reps))),
-                      var_z = rep(reps*2))
-
-
-legend <- dummy_dists %>%  density_dumbbell_plot(y_var = var_z, pct_change_text = F) + xlim(c(-1,6))
-
-y_position = nrow(dummy_dists) - 0.1
-
-(legend_annotated <- legend + 
-    
-    geom_text( 
-  aes(label = "Percentage gap\nbetween medians"),
-      x = 2.6,
-  position = position_nudge(x = 0, y = -0.3),
-  colour = alpha(yellow, 0.5),
-  family = font_regular) +
-  
-  geom_curve(aes(x = 2, xend = 0.5, y = y_position, yend = y_position - 0.2),
-             curvature = 0.3, color = blue) +
-    
-  annotate("text", x = 1, y = y_position - 0.4, label = "Median female salary", color = blue) +
-  
-  geom_curve(aes(x = 4.7, xend = 3.2, y = y_position - 0.2, yend = y_position),
-             curvature = 0.3, color = yellow) +
-    
-    labs(subtitle = "How to read the plots:") +
-    
-    theme(axis.text.x  = element_blank(),
-          plot.margin = grid::unit(c(0,5,0,10), "mm")))
 
 
 (age <- density_dumbbell_plot(df_filt, age) + 
@@ -217,21 +179,124 @@ age + experience + industry + education + plot_layout(ncol = 2) +
   theme(plot.background = element_rect(fill = background, colour = NA),
         plot.title = element_markdown(colour = text_colour, family = font_bold, size = 30))
 
+
+#### legend
+reps <- 1000000
+
+dummy_dists <- tibble(annual_salary = c(rnorm(reps,3),
+                                        rnorm(reps,2.2)), 
+                      gender = factor(c(rep("Man", reps), rep("Woman", reps))),
+                      var_z = rep(reps*2))
+
+
+legend <- dummy_dists %>%  
+  density_dumbbell_plot(y_var = var_z, pct_change_text = F) + 
+  xlim(c(-1,6))
+
+y_position_neg = nrow(dummy_dists) - 0.1
+y_position_pos = nrow(dummy_dists) + 0.4
+
+female_x <- 0.1
+male_x <- 5.1
+x_difference <- 1.7
+y_drop <- 0.5
+y_raise <- 0.3
+
+
+(legend_annotated <- legend + 
+    
+    geom_text( 
+      aes(label = paste0("\'+'*chi*\'%'")),
+      x = 2.6,
+      position = position_nudge(x = 0, y = -0.25),
+      colour = alpha(yellow, 0.5),
+      family = font_regular, parse = T) +
+    
+    # female distribution
+    geom_curve(aes(x = female_x, xend = female_x + x_difference, y = y_position_pos + y_raise, yend = y_position_pos),
+               curvature = -0.4, 
+               
+               arrow = arrow(length = unit(0.03, "npc")),
+               
+               color = text_colour) +
+    
+    annotate("text", x = female_x, y = y_position_neg - 0.8, label = "Median\nfemale\nsalary", color = blue) +
+    
+    
+    # female median
+    geom_curve(aes(x = female_x, xend = female_x + x_difference, y = y_position_neg - y_drop, yend = y_position_neg),
+               curvature = -0.4, 
+               
+               arrow = arrow(length = unit(0.03, "npc")),
+               
+               color = text_colour) +
+    
+    annotate("text", x = female_x, y = y_position_neg - 0.8, label = "Median\nfemale\nsalary", color = blue) +
+    
+    # male median
+    geom_curve(aes(x = male_x , xend = male_x - x_difference, y = y_position_neg - y_drop, yend = y_position_neg),
+               curvature = 0.4, 
+               arrow = arrow(length = unit(0.03, "npc")),
+               
+               color = text_colour) +
+    
+    annotate("text", x = male_x, y = y_position_neg - 0.8, label = "Median\nmale\nsalary", color = yellow) +
+    
+    # pct change
+    geom_curve(aes(x = 2.7, xend = 2.7, y = y_position_neg -1, yend = y_position_neg - 0.35),
+               curvature = 0.2,
+               arrow = arrow(length = unit(0.03, "npc")),
+               color = text_colour) +
+    
+    annotate("text", x = 2.7, y = y_position_neg - 1.3, 
+             label = "Percentage difference\n between male and\nfemale median", color = text_colour) +
+    
+    labs(subtitle = "") +
+    
+    annotate("text", x = 2.7, y = nrow(dummy_dists) + 1, 
+             label = "How to read\nthe plots:", color = text_colour, size = 7) +
+    
+    theme(axis.text.x  = element_blank(),
+          plot.margin = grid::unit(c(-10,5,-10,3), "mm"),
+          panel.grid.major.x = element_blank(),
+          plot.background = element_rect(fill = alpha(text_colour,0.05), color = NA),
+          panel.background = element_rect(fill = alpha(text_colour,0.05), color = NA)))
+
+
+
+intro_text <- ggplot() +
+  geom_textbox(aes(x = -3, y = 1, 
+                   label = "The **Ask A Manager Survey 2021** collected information on salaries from more than 24,000 people in April-May 2021.<br><br>
+                The data presented here only includes respondents who provided their salary in USD, CAD, or GBP - these were then standardised to USD. Respondents were filtered to only include <span style = 'color:#278B9AFF;'>**women**</span> and <span style = 'color:#D8AF39FF;'>**men**</span> over the age of 18.<br><br>
+                *n*=23,714 except for the **Industry** plot, which only shows data for the top six industries by number of responses (*n*=12,876)."), 
+               family = "Roboto", size = 4, 
+               colour = NA, fill = NA, text.colour = text_colour,
+               width = grid::unit(0.95, "npc"),
+               halign = 0.5, lineheight = 1.5) +
+  theme_void() +
+  theme(plot.margin = grid::unit(c(-10,5,-10,3), "mm"),
+        plot.background = element_rect(fill = alpha(text_colour,0.05), color = NA),
+        panel.background = element_rect(fill = alpha(text_colour,0.05), color = NA))
+
+
+
+### assembling entire plot
 layout <- "
-####AA
-BBBCCC
-BBBCCC
-DDDEEE
-DDDEEE
+FFBBBBCCCC
+FFBBBBCCCC
+AADDDDEEEE
+AADDDDEEEE
 "
 
 legend_annotated + age + experience + industry + education + plot_layout(design = layout) +
-  plot_annotation(title = "test") &
+  intro_text +
+  #inset_element(intro_text, -2, 1.5, -1.5, 2.35) +
+  plot_annotation(title = "**Ask A Manager Survey 2021:** exploring the gender pay gap") &
   theme(plot.background = element_rect(fill = background, colour = NA),
-        plot.title = element_markdown(colour = text_colour, family = font_bold, size = 30)) 
-
+        plot.title = element_markdown(colour = text_colour, family = "Roboto", size = 45, hjust = 0, margin = margin(15,0,15,-10)),
+        plot.margin = margin(5,5,5,5,"mm"))
 
 ggsave(filename =  paste0("temp-", format(Sys.time(), "%Y%m%d_%H%M%S"), ".png"), 
        device = NULL, path = here("Temp plots"),
-       dpi = 320, width = 14, height = 16)
+       dpi = 320, width = 17, height = 14)
 
